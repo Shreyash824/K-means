5b5fc80399fef45089d6bba9b55edecea094eb84
Shreyash824 <123435974+Shreyash824@users.noreply.github.com>
2026-07-10 20:22:42 +0530

    Refactor CustomerSegmentation class methods
    Refactor customer segmentation methods and add silhouette score calculation for optimal K.

--- model.py
+++ model.py
@@ -28,14 +28,45 @@ class CustomerSegmentation:
             self.model.cluster_centers_,
             self.model.inertia_
         )
+        from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
 
-    def elbow_method(self, df, max_clusters=10):
+
+class CustomerSegmentation:
+
+    def __init__(self, n_clusters=3, random_state=42):
+        self.n_clusters = n_clusters
+        self.random_state = random_state
+
+    def fit(self, df):
 
         X = df[["Annual_Spending", "Order_Count"]]
 
+        model = KMeans(
+            n_clusters=self.n_clusters,
+            random_state=self.random_state,
+            n_init=10
+        )
+
+        df = df.copy()
+
+        df["Cluster"] = model.fit_predict(X)
+
+        return (
+            df,
+            model.cluster_centers_,
+            model.inertia_
+        )
+
+    def find_optimal_k(self, df, max_clusters=10):
+
+        X = df[["Annual_Spending", "Order_Count"]]
+
+        best_k = 2
+        best_score = -1
         inertia = []
 
-        for k in range(1, max_clusters + 1):
+        for k in range(2, max_clusters + 1):
 
             model = KMeans(
                 n_clusters=k,
@@ -43,8 +74,16 @@ class CustomerSegmentation:
                 n_init=10
             )
 
-            model.fit(X)
+            labels = model.fit_predict(X)
 
             inertia.append(model.inertia_)
 
-        return inertia
+            score = silhouette_score(X, labels)
+
+            if score > best_score:
+                best_score = score
+                best_k = k
+
+        return best_k, best_score, inertia
+
+    
